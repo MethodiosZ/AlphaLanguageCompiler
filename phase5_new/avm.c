0d1b7b61e6773edb9d5b7b744839e9e00b0cc8fc
MethodiosZ <methzaxa@gmail.com>
2024-05-25 16:10:29 +0300

    stuff for 5

--- phase5_new/avm.c
+++ phase5_new/avm.c
@@ -1,5 +1,15 @@
 #include "avm.h"
 
+avm_memcell stack[AVM_STACKSIZE];
+unsigned char   executionFinished = 0;
+unsigned        pc = 0;
+unsigned        currLine = 0;
+unsigned        codeSize = 0;
+instruction     *code = (instruction*)0;
+avm_memcell     ax, bx, cx;
+avm_memcell     retval;
+unsigned        top, topsp;
+
 static void avm_initstack(){
     for(unsigned i=0;i<AVM_STACKSIZE;++i){
         AVM_WIPEOUT(stack[i]);
@@ -48,4 +58,78 @@ void avm_tablebucketsdestroy(avm_table_bucket **p){
         }
         p[i] = (avm_table_bucket*)0;
     }
+}
+
+avm_memcell *avm_translate_operand(vmarg *arg, avm_memcell *reg){
+    switch(arg->type){
+        case global_a: return &stack[AVM_STACKSIZE-1-arg->val];
+        case local_a: return &stack[topsp-arg->val];
+        case formal_a: return &stack[topsp+AVM_STACKENV_SIZE+1+arg->val];
+        case retval_a: return &retval;
+        case number_a: {
+            reg->type = number_m;
+            reg->data.numVal = consts_getnumber(arg->val);
+            return reg;
+        }
+        case string_a: {
+            reg->type = string_m;
+            reg->data.strVal =  strdup(consts_getstring(arg->val));
+            return reg;
+        }
+        case bool_a: {
+            reg->type = bool_m;
+            reg->data.boolVal = arg->val;
+            return reg;
+        }
+        case nil_a: {
+            reg->type=nil_m;
+            return reg;
+        }
+        case userfunc_a: {
+            reg->type = userfunc_m;
+            reg->data.funcVal = arg->val;
+            return reg;
+        }
+        case libfunc_a: {
+            reg->type = libfunc_m;
+            reg->data.libfuncVal = linfuncs_getused(arg->val);
+            return reg;
+        }
+        default: assert(0);
+    }
+}
+
+void execute_cycle(){
+    if (executionFinished) return;
+    else if (pc == AVM_ENDING_PC){
+        executionFinished =1;
+        return;
+    } 
+    else {
+        assert(pc < AVM_ENDING_PC);
+        instruction *t = code + pc;
+        assert(t->opcode >=  0 && t->opcode <= AVM_MAX_INSTRUCTIONS);
+        if (t->srcLine) currLine = t->srcLine;
+        unsigned oldPC = pc;
+        (*executeFuncs[t->opcode])(t);
+        if (pc==oldPC) ++pc;
+    }
+}
+
+void avm_memcellclear(avm_memcell*m){
+    if(m->type != undef_m){
+        memclear_func_t f = memclearFuncs[m->type];
+        if(f) (*f)(m);
+        m->type = undef_m;
+    }
+}
+
+void memclear_string(avm_memcell *m){
+    assert(m->data.strVal);
+    free(m->data.strVal);
+}
+
+void memclear_table(avm_memcell *m){
+    assert(m->data.tableVal);
+    avm_tabledecrefcounter(m->data.tableVal);
 }
--- phase5_new/avm.h
+++ phase5_new/avm.h
@@ -1,10 +1,53 @@
 #include "exec.h"
 
-#define AVM_STACKSIZE   4096
-#define AVM_WIPEOUT(m)  memset(&(m),0,sizeof(m))
-#define AVM_TABLE_HASHSIZE  211
+#define AVM_STACKSIZE 4096
+#define AVM_WIPEOUT(m) memset(&(m),0,sizeof(m))
+#define AVM_TABLE_HASHSIZE 211
+#define AVM_ENDING_PC codeSize
+#define AVM_STACKENV_SIZE 4
+#define AVM_MAX_INSTRUCTIONS (unsigned) nop_v
 
-avm_memcell stack[AVM_STACKSIZE];
+execute_func_t executeFuncs[] = {
+    execute_assign,
+    execute_add,
+    execute_sub,
+    execute_mul,
+    execute_div,
+    execute_mod,
+    execute_uminus,
+    execute_and,
+    execute_or,
+    execute_not,
+    execute_jeq,
+    execute_jne,
+    execute_jle,
+    execute_jge,
+    execute_jlt,
+    execute_jgt,
+    execute_call,
+    execute_pusharg,
+    execute_funcenter,
+    execute_funcexit,
+    execute_newtable,
+    execute_tablegetelem,
+    execute_tablesetelem,
+    execute_nop
+};
+
+memclear_func_t memclearFuncs[] = {
+    0,
+    0,
+    memclear_string,
+    0,
+    memclear_table,
+    0,
+    0,
+    0,
+    0
+};
+
+typedef void (*execute_func_t)(instruction*);
+typedef void (*memclear_func_t)(avm_memcell*); 
 
 typedef enum avm_memcell_t{
     number_m, string_m, bool_m, table_m, userfunc_m, libfunc_m,
@@ -47,3 +90,41 @@ void avm_tablebucketsinit(avm_table_bucket **p);
 void avm_memcellclear(avm_memcell *m);
 void avm_tablebucketsdestroy(avm_table_bucket **p);
 avm_memcell *avm_translate_operand(vmarg *arg, avm_memcell *reg);
+void execute_cycle();
+void avm_callsaveenvironment();
+double  consts_getnumber(unsigned index);
+char *consts_getstring(unsigned index);
+char *linfuncs_getused(unsigned index);
+userfunc *userfuncs_getfunc(unsigned index);
+
+typedef void (*library_func_t)();
+library_func_t avm_getlibraryfunc(char *id);
+void avm_calllibfunc(char *id);
+
+void execute_assign(instruction *instr);
+void execute_add(instruction *instr);
+void execute_sub(instruction *instr);
+void execute_mul(instruction *instr);
+void execute_div(instruction *instr);
+void execute_mod(instruction *instr);
+void execute_uminus(instruction *instr);
+void execute_and(instruction *instr);
+void execute_or(instruction *instr);
+void execute_not(instruction *instr);
+void execute_jeq(instruction *instr);
+void execute_jne(instruction *instr);
+void execute_jle(instruction *instr);
+void execute_jge(instruction *instr);
+void execute_jlt(instruction *instr);
+void execute_jgt(instruction *instr);
+void execute_call(instruction *instr);
+void execute_pusharg(instruction *instr);
+void execute_funcenter(instruction *instr);
+void execute_funcexit(instruction *instr);
+void execute_newtable(instruction *instr);
+void execute_tablegetelem(instruction *instr);
+void execute_tablesetelem(instruction *instr);
+void execute_nop(instruction *instr);
+
+memclear_string(avm_memcell *m);
+memclear_table(avm_memcell *m);
